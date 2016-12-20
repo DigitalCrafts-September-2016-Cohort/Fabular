95fafd2ef1cd7e6b49ea90b876063b578dcb9bab
Keyur Patel <ekeyur@gmail.com>
2016-12-20 15:14:04 -0500

    Added audio

--- static/fabular.js
+++ static/fabular.js
@@ -1,5 +1,19 @@
 var app = angular.module('fabular', ['ui.router']);
 
+function textToSpeak(msg, idx) {
+	if (typeof msg !== 'string') {
+		throw new TypeError('Expected to say a string.');
+	}
+	var y = window.speechSynthesis;
+	if (!y) {
+		return console.warn('Your browser does not support `speechSynthesis` yet.');
+	}
+	var s = new SpeechSynthesisUtterance(msg);
+	s.voice = y.getVoices()[idx || 0];
+	y.speak(s);
+}
+
+
 app.config(function($stateProvider,$urlRouterProvider){
   $stateProvider
   .state({
