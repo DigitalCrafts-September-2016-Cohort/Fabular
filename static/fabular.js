9cd58de55158efe89094f2f1c0eb1a003bd4d258
Autumn Coleman <autumn.eve.coleman@gmail.com>
2016-12-19 23:12:35 -0500

    Added flex to styles, disabled on wrong click

--- static/fabular.js
+++ static/fabular.js
@@ -24,11 +24,26 @@ return service;
 });
 
 app.controller('fabularController', function($scope,fabularService) {
-// fruits = ['apple','banana','berries','cranberries','grapes','kiwi','pineapple','strawberry'];
-fabularService.getThings().success(function(data){
-  console.log(data);
-  $scope.item = data[Math.floor((Math.random() * 3))];
-  $scope.things = data;
-});
+  // fruits = ['apple','banana','berries','cranberries','grapes','kiwi','pineapple','strawberry'];
+  fabularService.getThings().success(function(data){
+    console.log(data);
+    $scope.item = data[Math.floor((Math.random() * 3))];
+    $scope.things = data;
+    $scope.click = function(someThing) {
+      console.log("click!");
+      $scope.checkDisabled = function(item) {
+        if (item !== $scope.item) {
+          return true;
+        }
+      };
+      if (someThing === $scope.item) {
+        console.log("Same");
+      }
+      else {
+        console.log("Not same");
+      }
+    };
+  });
+
 
 });
