b2c20a0bc1babefdbf3eb7f4962678fd9605a80a
Keyur Patel <ekeyur@gmail.com>
2016-12-23 15:10:21 -0500

    fixed I to i as the image was not showing up

--- static/fabular.js
+++ static/fabular.js
@@ -46,7 +46,7 @@ return service;
 
 
 app.controller('fabularController', function($scope, $timeout,$stateParams, $rootScope, $state, fabularService) {
-	var i_obj = {"name" : "I", "wobble" : "false"};
+	var i_obj = {"name" : "i", "wobble" : "false"};
 	var ask_obj = {"name" : "askfor", "wobble" : "false"};
 	var want_obj = {"name" : "want", "wobble" : "false"};
 	var obj_1 = {"name" : "1", "wobble" : "false"};
@@ -54,6 +54,7 @@ app.controller('fabularController', function($scope, $timeout,$stateParams, $roo
 	var obj_3 = {"name" : "3", "wobble" : "false"};
 	$scope.countWins = 0;
   chelevel = parseInt($stateParams.level);
+	$scope.basket = [];
   $scope.Again = function(){
   	fabularService.getThings().success(function(data){
     $scope.currentIndex = 0;
@@ -94,8 +95,10 @@ app.controller('fabularController', function($scope, $timeout,$stateParams, $roo
 		$scope.questionArray.forEach(function(value){
 			textToSpeak(value.name);
 		});
+
 		$scope.clicked = function(option) {
 			if (option.name === $scope.expectedResult[$scope.currentIndex].name) {
+
 				$scope.currentIndex += 1;
 				textToSpeak(option.name);
 				$scope.resultLink.push(option);
@@ -138,6 +141,11 @@ app.controller('fabularController', function($scope, $timeout,$stateParams, $roo
 			if($scope.expectedResult.length === $scope.resultLink.length){
 				$scope.resultLink.forEach(function(value){
 					textToSpeak(value.name);
+					if(value.name in data.name){
+						for(let i=0;i<r;i++){
+							$scope.basket.push(value.name);
+						}
+					}
 				});
 				textToSpeak("Good Job, Would you like to play again?");
 			}
