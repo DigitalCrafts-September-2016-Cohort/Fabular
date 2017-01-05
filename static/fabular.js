1d1c2d2db72193633c815d975d61a280b11b7fb6
Autumn Coleman <autumn.eve.coleman@gmail.com>
2017-01-05 17:32:17 -0500

    Latest working verison of web app

--- static/fabular.js
+++ static/fabular.js
@@ -26,28 +26,24 @@ app.config(function($stateProvider,$urlRouterProvider){
     url : '/things',
     templateUrl : 'things.html',
     controller : 'fabularController'
-  })
-	.state({
-    name : 'settings',
-    url : '/settings',
-    templateUrl : 'settings.html',
-    controller : 'fabularController'
   });
   $urlRouterProvider.otherwise('/things');
 });
 
 //Factory
 app.factory('fabularService',function($http){
-  var service = {};
-  service.getThings = function(){
-    var url = '/things';
-    return $http({
-      method : 'GET',
-      url : url
-    });
-  };
+	var service = {};
 
-return service;
+	service.getThings = function(category){
+		console.log(category);
+		var url = '/things/' + category;
+		return $http({
+			method : 'GET',
+			url : url,
+			category: category,
+		});
+	};
+	return service;
 });
 
 //Controllers
@@ -59,16 +55,17 @@ app.controller('fabularController', function($scope, $timeout,$stateParams, $roo
 	var obj_1 = {"name" : "1", "wobble" : "false"};
 	var obj_2 = {"name" : "2", "wobble" : "false"};
 	var obj_3 = {"name" : "3", "wobble" : "false"};
-
+	var r;
+	$scope.levels = [1,2,3,4,5];
 	$scope.clickedStatement = false;
+	$scope.settings = 'noshow';
 	var inBasket = [];
-
+	// var category = $scope.category;
+	$scope.category = 'animals';
 	$scope.clickedBasket = function() {
 		if($scope.clickedStatement === false){
       $scope.clickedStatement = true;
-			console.log($scope.clickedStatement);
 		inBasket.sort();
-
 		var item = null;
 		var cnt = 0;
 		for (var i = 0; i < inBasket.length + 1; i++) {
@@ -84,8 +81,6 @@ app.controller('fabularController', function($scope, $timeout,$stateParams, $roo
 		  }
 		} else if($scope.clickedStatement === true) {
 			$scope.clickedStatement = false;
-			console.log($scope.clickedStatement);
-
 		}
 	};
 
@@ -94,126 +89,142 @@ app.controller('fabularController', function($scope, $timeout,$stateParams, $roo
 	//Game wrapped in play again function
 	$scope.levelChange = function(value){
 		chelevel = parseInt(value);
+		$scope.level = value;
+		$scope.settings = 'noshow';
 		$scope.Again();
 	};
+	$scope.setCategory = function(value){
+		$scope.category = value;
+		console.log(value);
+		$scope.settings = 'noshow';
+		$scope.Again();
+
+	};
+	$scope.setSettings = function(){
+		$scope.settings = 'show';
+	};
+
   $scope.Again = function(){
-  	fabularService.getThings().success(function(data){
-    $scope.currentIndex = 0;
-    $scope.resultLink = [];
-		//Selects random number through 3 for levels with counting
-    var r = (Math.floor(Math.random() * 3) + 1).toString();
-		//Converts random number into an object
-		var r_num = {"name" : r, "wobble" : "false"};
-		//Selects single item from data array using random index number
-    $scope.item = data[Math.floor((Math.random() * 3))];
-		//Builds question array with 'ask' image and $scope.item
-    $scope.questionArray = [ask_obj,$scope.item];
-    //Sets the values of options arrays and expected result arrays depending on level selected:
-    if(chelevel === 1){
-      $scope.optionsArray = data;
-      $scope.expectedResult = [$scope.item];
-    }else if (chelevel === 2){
-      data.unshift(want_obj);
-      $scope.optionsArray = data;
-      $scope.expectedResult = [want_obj, $scope.item];
-      $scope.expectedResult = [want_obj,$scope.item];
-    }else if (chelevel === 3){
-      data.unshift(i_obj,want_obj);
-      $scope.optionsArray = data;
-      // console.log($scope.optionsArray);
-			console.log($scope.resultLink);
+		$scope.resultLink = [];
+  	fabularService.getThings($scope.category).success(function(data){
+			$scope.currentIndex = 0;
+			//Selects random number through 3 for levels with counting
+	    r = (Math.floor(Math.random() * 3) + 1).toString();
+			//Converts random number into an object
+			var r_num = {"name" : r, "wobble" : "false"};
+			//Selects single item from data array using random index number
+	    $scope.item = data[Math.floor((Math.random() * 3))];
+			console.log($scope.item);
+			//Builds question array with 'ask' image and $scope.item
+	    $scope.questionArray = [ask_obj,$scope.item];
+	    //Sets the values of options arrays and expected result arrays depending on level selected:
+	    if(chelevel === 1){
+	      $scope.optionsArray = data;
+	      $scope.expectedResult = [$scope.item];
+	    }else if (chelevel === 2){
+	      data.unshift(want_obj);
+	      $scope.optionsArray = data;
+	      $scope.expectedResult = [want_obj, $scope.item];
+	      $scope.expectedResult = [want_obj,$scope.item];
+	    }else if (chelevel === 3){
+	      data.unshift(i_obj,want_obj);
+	      $scope.optionsArray = data;
+	      // console.log($scope.optionsArray);
+				console.log($scope.resultLink);
 
-      $scope.expectedResult = [i_obj,want_obj,$scope.item] ;
-    }else if (chelevel === 4){
-      data.unshift(i_obj,want_obj,obj_1,obj_2,obj_3);
-      $scope.questionArray = [ask_obj,r_num,$scope.item];
-      $scope.optionsArray = data;
-      $scope.expectedResult = [i_obj,want_obj,r_num,$scope.item] ;
-    }else if (chelevel === 5){
-			please_obj = {"name" : "please", "wobble" : "false"};
-      data.unshift(i_obj,want_obj,obj_1,obj_2,obj_3);
-      data.push(please_obj);
-      $scope.optionsArray = data;
-      $scope.questionArray = [ask_obj,r_num,$scope.item];
-      $scope.expectedResult = [i_obj,want_obj,r_num,$scope.item,please_obj] ;
-    }
-		$scope.questionArray.forEach(function(value){
-			// textToSpeak(value.name);
+	      $scope.expectedResult = [i_obj,want_obj,$scope.item] ;
+	    }else if (chelevel === 4){
+	      data.unshift(i_obj,want_obj,obj_1,obj_2,obj_3);
+	      $scope.questionArray = [ask_obj,r_num,$scope.item];
+	      $scope.optionsArray = data;
+	      $scope.expectedResult = [i_obj,want_obj,r_num,$scope.item] ;
+	    }else if (chelevel === 5){
+				please_obj = {"name" : "please", "wobble" : "false"};
+	      data.unshift(i_obj,want_obj,obj_1,obj_2,obj_3);
+	      data.push(please_obj);
+	      $scope.optionsArray = data;
+	      $scope.questionArray = [ask_obj,r_num,$scope.item];
+	      $scope.expectedResult = [i_obj,want_obj,r_num,$scope.item,please_obj] ;
+	    }
+			$scope.questionArray.forEach(function(value){
+				// textToSpeak(value.name);
+			});
 		});
+	};
 
-//Click function
-		$scope.clicked = function(option) {
-			//Handles correct click events
-			if (option.name === $scope.expectedResult[$scope.currentIndex].name) {
-				$scope.currentIndex += 1;
-				// textToSpeak(option.name);
-				$scope.resultLink.push(option);
-				if(chelevel === 1 && $scope.currentIndex === 1){
-          $scope.optionsArray = [];
-        }
-				if(chelevel === 2 && $scope.currentIndex === 2){
-          $scope.optionsArray = [];
-        }
-        if(chelevel === 3 && $scope.currentIndex === 3){
-          $scope.optionsArray = [];
-					inBasket.push($scope.resultLink[$scope.currentIndex -1].name);
-        }
-        if(chelevel === 4 && $scope.currentIndex === 4){
-					$scope.optionsArray = [];
-        }
-				//Split levels because of splice in 153
-				if((chelevel === 4 || chelevel === 5) && ($scope.currentIndex === 3 || $scope.currentIndex === 4)){
-					$scope.optionsArray.splice(0,3);
+	//Click function
+	$scope.clicked = function(option) {
+		//Handles correct click events
+		if (option.name === $scope.expectedResult[$scope.currentIndex].name) {
+			$scope.currentIndex += 1;
+			// textToSpeak(option.name);
+			$scope.resultLink.push(option);
+			if(chelevel === 1 && $scope.currentIndex === 1){
+        $scope.optionsArray = [];
+      }
+			if(chelevel === 2 && $scope.currentIndex === 2){
+        $scope.optionsArray = [];
+      }
+      if(chelevel === 3 && $scope.currentIndex === 3){
+        $scope.optionsArray = [];
+				inBasket.push($scope.resultLink[$scope.currentIndex -1].name);
+      }
+      if(chelevel === 4 && $scope.currentIndex === 4){
+				$scope.optionsArray = [];
+      }
+			//Split levels because of splice in 153
+			if((chelevel === 4 || chelevel === 5) && ($scope.currentIndex === 3 || $scope.currentIndex === 4)){
+				$scope.optionsArray.splice(0,3);
+			}
+      if(chelevel === 5 && $scope.currentIndex === 5){
+        $scope.optionsArray.pop();
+      }
+			if ($scope.currentIndex === 1 || $scope.currentIndex === 2){
+			$scope.optionsArray.splice($scope.optionsArray.indexOf(option), 1);
+		}
+	}
+    else {
+			//Handles incorrect click events with verbal prompt for correct option
+			// textToSpeak("Please press  "+$scope.expectedResult[$scope.currentIndex].name);
+			var obj = $scope.optionsArray.filter(function(option){
+					return option.name === $scope.expectedResult[$scope.currentIndex].name;
+				});
+				//Wobbles correct button
+				obj[0].wobble = true;
+				$timeout(function () {
+					$scope.optionsArray.forEach(function(a){
+						a.wobble = false;
+					});
+				}, 1000);
+		}
+		//When user creates correct sentence
+		//When user creates correct sentence
+		if($scope.expectedResult.length === $scope.resultLink.length){
+			//textToSpeak function reads the sentence
+			$scope.resultLink.forEach(function(value){
+				// textToSpeak(value.name);
+				});
+			//Pushes 'x' number of prompt items into reward basket for levels 4 and 5
+			if(chelevel === 5){
+				for(let j=0;j<r;j++){
+					inBasket.push($scope.resultLink[$scope.currentIndex -2].name);
 				}
-        if(chelevel === 5 && $scope.currentIndex === 5){
-          $scope.optionsArray.pop();
-        }
-				if ($scope.currentIndex === 1 || $scope.currentIndex === 2){
-				$scope.optionsArray.splice($scope.optionsArray.indexOf(option), 1);
+			}else if(chelevel === 4) {
+				for(let j=0;j<r;j++){
+					inBasket.push($scope.resultLink[$scope.currentIndex -1].name);
+				}
+		//Pushes prompt item into reward basket
+			}else{
+				inBasket.push($scope.resultLink[$scope.currentIndex -1].name);
 			}
 		}
-      else {
-				//Handles incorrect click events with verbal prompt for correct option
-				// textToSpeak("Please press  "+$scope.expectedResult[$scope.currentIndex].name);
-				var obj = $scope.optionsArray.filter(function(option){
-						return option.name === $scope.expectedResult[$scope.currentIndex].name;
-					});
-					//Wobbles correct button
-					obj[0].wobble = true;
-					$timeout(function () {
-						$scope.optionsArray.forEach(function(a){
-							a.wobble = false;
-						});
-					}, 1000);
-			}
-			//When user creates correct sentence
-			//When user creates correct sentence
-			if($scope.expectedResult.length === $scope.resultLink.length){
-				//textToSpeak function reads the sentence
-				$scope.resultLink.forEach(function(value){
-					// textToSpeak(value.name);
-					});
-				//Pushes 'x' number of prompt items into reward basket for levels 4 and 5
-				if(chelevel === 5){
-					for(let j=0;j<r;j++){
-						inBasket.push($scope.resultLink[$scope.currentIndex -2].name);
-					}
-				}else if(chelevel === 4) {
-					for(let j=0;j<r;j++){
-						inBasket.push($scope.resultLink[$scope.currentIndex -1].name);
-					}
-			//Pushes prompt item into reward basket
-				}else{
-					inBasket.push($scope.resultLink[$scope.currentIndex -1].name);
-				}
-			}
-			// textToSpeak("Good Job, Would you like to play again?");
-		};
+		// textToSpeak("Good Job, Would you like to play again?");
 		$scope.basket = inBasket;
-  });
-};
-$scope.Again();
-$scope.playAgain = function(){
-$scope.Again();
-};
+	};
+
+	$scope.Again();
+	$scope.playAgain = function(){
+		$scope.resultLink = [];
+		$scope.Again();
+	};
 });
