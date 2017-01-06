4ad96d1144b7252c298a01e42f7d7c4bc298be5e
Che B <cnakamura336@gmail.com>
2017-01-06 10:51:54 -0500

    fix js file

--- static/fabular.js
+++ static/fabular.js
@@ -246,105 +246,6 @@ app.controller('fabularController', function($scope, $timeout,$stateParams, $roo
 				    document.getElementById("mask").className = "mask masked";
 						// $(".animated").removeClass("animated");
 						// $(".animated").addClass("animated");
-
-	// popup tutorial
-	$scope.tutorStatement = false;
-	$scope.tutorial = function() {
-		$scope.tutorStatement = true;
-		if($scope.tutorStatement === true){
-			// popup
-
-			var tour = {
-			  id: "hello-hopscotch-a",
-			  steps: [{
-			    title: "Application Menu",
-			    content: "Click here to open and close the appliation menu",
-			    target: "btn-menu",
-			    placement: "bottom",
-					yOffset : 100,
-			    xOffset: 250,
-			    onShow: function() {
-						$(".prompt").removeClass("animated");
-						$(".prompt").addClass("z-one");
-						$(".promptPic").addClass("highLight");
-			      document.getElementById("btn-menu").style.zIndex = 9999999999999;
-						// document.getElementById("entire").style.zIndex = 1;
-						console.log("On 1");
-					},
-			    onNext: function() {
-						$(".prompt").removeClass("highLight");
-						$(".prompt").addClass("animated");
-						$(".prompt").removeClass("z-one");
-			      document.getElementById("btn-menu").style.zIndex = 0;
-						// document.getElementById("entire").style.zIndex = 1;
-						console.log("Off 1");
-			    }
-			  }, {
-			    title: "System Information",
-			    content: "Click here to acces info about the current state of the system",
-			    target: "btn-system",
-			    placement: "bottom",
-			    xOffset: 850,
-					yOffset: 100, // this will set the left - right
-			    arrowOffset: 260,
-			    onShow: function() {
-						$(".menu").addClass("z-one");
-			      document.getElementById("btn-system").style.zIndex = 9999999999999;
-						console.log("On 2");
-			    },
-			    onNext: function() {
-						$(".menu").removeClass("z-one");
-			      document.getElementById("btn-system").style.zIndex = 0;
-						console.log("Off 2");
-			    }
-			  }, {
-			    title: "User Information",
-			    content: "Click here to logout, switch users, and opt out of the beta",
-			    target: "btn-user",
-			    placement: "bottom",
-			    xOffset: 200, // this will set the left - right
-			    yOffset: 400,
-			    arrowOffset: 130,
-			    onShow: function() {
-						$(".options").addClass("highLight");
-						$(".options").addClass("animated");
-						$(".options").addClass("z-one");
-			      document.getElementById("btn-user").style.zIndex = 9999999999999;
-						console.log("On 3");
-			    },
-			    onNext: function() {
-						$(".options").removeClass("highLight");
-						$(".options").addClass("animated");
-						$(".options").removeClass("z-one");
-						document.getElementById("btn-user").style.zIndex = 0;
-						console.log("Off 3");
-			    }
-			  },{
-			    title: "fourth",
-			    content: "fourth here to logout, switch users, and opt out of the beta",
-			    target: "btn-users",
-			    placement: "bottom",
-			    xOffset: 100, // this will set the left - right
-			    yOffset: 500,
-			    arrowOffset: 130,
-			    onShow: function() {
-						$(".basket").addClass("highLight");
-						$(".basket").removeClass("animated");
-						$(".basket").addClass("z-one");
-			      document.getElementById("btn-users").style.zIndex = 9999999999999;
-						console.log("On 4");
-			    },
-			    onNext: function() {
-						// $(".options").addClass("animated");
-			      document.getElementById("btn-users").style.zIndex = 0;
-						console.log("Off 4");
-			    }
-			  }],
-			  onStart: function() {
-			    document.getElementById("mask").className = "mask masked";
-					// $(".animated").removeClass("animated");
-					// $(".animated").addClass("animated");
-
 					console.log("on");
 			  },
 			  onEnd: function() {
@@ -355,17 +256,5 @@ app.controller('fabularController', function($scope, $timeout,$stateParams, $roo
 			};
 
 			hopscotch.startTour(tour, 0);
-		}
-	};
-						console.log("on");
-				  },
-				  onEnd: function() {
-				    document.getElementById("mask").className = "mask";
-						// $(".options").addClass("animated");
-						console.log("off");
-					}
-				};
-
-				hopscotch.startTour(tour, 0);
 		}
 	};
